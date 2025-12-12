db68b773d39607fec489ea7b9eb12193d5d14184
opragopa <opragopa1@gmail.com>
2025-12-12 06:16:48 +0300

    updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 
-# Добавляем текущую директорию в путь поиска модулей
 sys.path.append(os.path.dirname(__file__))
 
 from PySide6.QtWidgets import QApplication
@@ -10,18 +9,14 @@ from login_dialog import LoginDialog
 from views.main_window import MainWindow
 
 def main():
-    # ЕДИНСТВЕННЫЙ QApplication — создаётся здесь
     app = QApplication(sys.argv)
 
-    # Инициализация пользователей (если их ещё нет)
     init_default_users()
 
-    # Авторизация
     login_dialog = LoginDialog()
     if not login_dialog.exec():
         sys.exit(0)
 
-    # Запуск главного окна
     main_window = MainWindow(login_dialog.role)
     main_window.show()
     sys.exit(app.exec())
